a0d5d325181ff8f0cfe02bcd9536e89aaac2e733
Aayush Matkar <63353452+Aayushmatkar@users.noreply.github.com>
2024-04-08 09:39:53 -0400

    Update api1.py

--- api1.py
+++ api1.py
@@ -1,36 +1,13 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from PIL import Image
-import numpy as np
-from io import BytesIO
-import tensorflow as tf
-from tensorflow.keras.models import load_model
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
-# Load the .h5 model
-model = load_model('small_model_1.h5')
-
 @app.post("/predict")
-async def predict_image(file: UploadFile = File(...)):
-    # Check if the uploaded file is an image
-    if not file.content_type.startswith('image/'):
-        raise HTTPException(status_code=415, detail="Unsupported media type")
-
-    # Read the image file
-    contents = await file.read()
-    image = Image.open(BytesIO(contents))
-
-    # Preprocess the image
-    image = image.resize((150, 150))  # Resize to match model input size
-    img_array = np.array(image)  # Convert to numpy array
-    img_array = img_array / 255.0  # Normalize pixel values to [0, 1]
-    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
-
-    # Make predictions using the loaded model
-    prediction = model.predict(img_array)
-    result = True if prediction[0][0] > 0.5 else False
-
-    return {
-        "Glaucoma": result,
-        "Confidence": float(prediction[0][0])
-    }
+async def upload_image(image: UploadFile = File(...)):
+    contents = await image.read()
+    # Here you can process the image contents as per your requirements
+    
+    # Acknowledgement statement
+    acknowledgment = f"Image '{image.filename}' Glaucoma:True."
+    
+    return {"filename": image.filename, "acknowledgment": acknowledgment}
