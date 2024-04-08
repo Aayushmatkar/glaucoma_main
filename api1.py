80db1ad79b7280d11dc5314750e7bf79d4d38705
Aayush Matkar <63353452+Aayushmatkar@users.noreply.github.com>
2024-04-08 10:22:26 -0400

    Update api1.py

--- api1.py
+++ api1.py
@@ -1,13 +1,18 @@
+import random
 from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
-@app.post("/predict")
+@app.post("/upload/")
 async def upload_image(image: UploadFile = File(...)):
     contents = await image.read()
     # Here you can process the image contents as per your requirements
     
-    # Acknowledgement statement
-    acknowledgment = f"Image '{image.filename}' Glaucoma:True."
+    # Random acknowledgment statement
+    acknowledgments = [
+        f"Image '{image.filename}' Glaucoma : True",
+        f"Image '{image.filename}' Glaucoma : False",
+    ]
+    acknowledgment = random.choice(acknowledgments)
     
     return {"filename": image.filename, "acknowledgment": acknowledgment}
