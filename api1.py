332c0d6398d4babd009260d084195a5053a84d42
Aayush Matkar <63353452+Aayushmatkar@users.noreply.github.com>
2024-04-08 20:03:06 -0400

    Update api1.py

--- api1.py
+++ api1.py
@@ -3,6 +3,21 @@ from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
+# Setup CORS
+origins = [
+    "http://localhost:3000",  # Update this with the actual origin of your frontend
+    "https://glaucomaapi-2024-6fc75a38c6ac.herokuapp.com",
+    "https://www.e-hospital.ca",# Add additional origins if needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.post("/predict")
 async def upload_image(image: UploadFile = File(...)):
     contents = await image.read()
